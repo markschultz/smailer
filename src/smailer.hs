2d91bd159b56ffbbcc924efdb3d5e51ea45affe7
Mark Schultz <mxs802@case.edu>
2013-11-13 17:02:46 -0600

    database parsing and loaderio

--- src/smailer.hs
+++ src/smailer.hs
@@ -6,12 +6,17 @@ import Data.Monoid
 import System.Environment
 import Control.Monad
 import Data.Text as T
+import qualified Data.Text.Lazy as TL
 
 main = do
         port <- liftM read $ getEnv "PORT"
         mgKey <- getEnv "MAILGUN_API_KEY"
         mgLogin <- getEnv "MAILGUN_SMTP_LOGIN"
         let (_:mgDomain) = splitOn "@" $ T.pack mgLogin
+        dbc <- getEnv "DATABASE_URL"
+        let (auth:server) = splitOn "@" $ T.pack dbc
+        let (_:up:_) = splitOn "//" auth
+        putStrLn $ show up
         scotty port $ do
             get "/" $ do
                 html "Hello World!"
@@ -21,4 +26,8 @@ main = do
                 html $ mconcat [ "<h1>TO: ", to
                                , "</h1><br/><h1>FROM: ", txt
                                , "</h1>" ]
+            get "/db" $ do
+                html $ TL.fromStrict up
+            get "/loaderio-9204d6a37af2101e440254b90c29248a" $ do
+                text "loaderio-9204d6a37af2101e440254b90c29248a"
 
