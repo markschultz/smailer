ba22870055fcdfa1f5eeebec0c49b2bd6ea23a54
Mark Schultz <mxs802@case.edu>
2013-11-14 16:18:20 -0600

    test email sending and try out html templating

--- src/smailer.hs
+++ src/smailer.hs
@@ -3,36 +3,51 @@
 import Web.Scotty
 import Mailgun
 import Data.Monoid
+import Control.Monad.IO.Class (liftIO)
 import System.Environment
 import Control.Monad
 import Network.HTTP.Types
 import Data.Text as T
+import Data.Text.Encoding as T
+import Data.ByteString as B
+import Data.ByteString.Char8 as B8
 import qualified Data.Text.Lazy as TL
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 ((!))
+import Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+
+blaze = html . renderHtml
 
 main = do
         port <- liftM read $ getEnv "PORT"
         mgKey <- getEnv "MAILGUN_API_KEY"
         mgLogin <- getEnv "MAILGUN_SMTP_LOGIN"
-        let (_:mgDomain) = splitOn "@" $ T.pack mgLogin
+        let (_:mgDomain:_) = splitOn "@" $ T.pack mgLogin
         dbc <- getEnv "DATABASE_URL"
         let (auth:server) = splitOn "@" $ T.pack dbc
         let (_:up:_) = splitOn "//" auth
-        putStrLn $ show up
         scotty port $ do
-            get "/" $ do
-                html "Hello World!"
-            get "/loaderio-9204d6a37af2101e440254b90c29248a" $ do
+            get "/" $ html "Hello World!"
+            get "/loaderio-9204d6a37af2101e440254b90c29248a" $
                 text "loaderio-9204d6a37af2101e440254b90c29248a"
-            get "/:to/:text" $ do
-                to <- param "to"
-                txt <- param "text"
-                html $ mconcat [ "<h1>TO: ", to
-                               , "</h1><br/><h1>FROM: ", txt
-                               , "</h1>" ]
-            get "/db" $ do
-                html $ TL.fromStrict up
+            get "/db" $ html $ TL.fromStrict up
             post "/emails" $ do
                 f <- param "from"
                 s <- param "subject"
                 text $ mconcat ["from: " , f , "\nsubject: " , s]
+            get "/send/:to/:subj" $ do
+                subject <- param "subj"
+                to <- param "to"
+                resp <- liftIO $ sendEmail (to <> "")
+                    ("notification@" <> T.encodeUtf8 mgDomain)
+                    subject "This is a test email."
+                    (B8.pack mgKey) (T.unpack mgDomain)
+                blaze $ H.html $ do
+                    H.h1 $ H.toHtml $ "Subject: " <>
+                        T.decodeLatin1 subject
+                    H.h1 $ H.toHtml $ "To: " <> T.decodeLatin1 to
+                    H.br
+                    H.h1 "Response:"
+                    H.p $ H.toHtml $ T.pack resp
 
